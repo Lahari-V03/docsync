5a30f2da4508fd53a9f0b76b6d30c172912b30fb
Lahari V <laharivuppalapati@gmail.com>
2026-06-25 01:07:26 -0400

    corrected run_pipeline.py

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,6 +1,6 @@
 from connectors.pdf_connector import run as run_pdf
 from connectors.scanned_pdf_connector import run as run_scanned_pdf
-from connectors.json_connector import run as run_json_
+from connectors.json_connector import run as run_json
 
 if __name__ == "__main__":
     print("Running PDF connector...")
